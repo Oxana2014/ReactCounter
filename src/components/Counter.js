9317c866112250ec1f3140b9156ef48790a6deaf
Oksana Kormilina <keysb@mail.ru>
2024-02-08 14:14:59 +0400

    Use redux store and dispatch actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,29 @@
+import {useSelector, useDispatch } from 'react-redux'
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch()
+const counter = useSelector((state) => state.counter)
+
+function handleIncrement() {
+  dispatch({type: 'increment'})
+}
+
+function handleDecrement() {
+  dispatch({type: 'decrement'})
+}
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}> {counter} </div>
+      <div>
+        <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={handleIncrement}>Increment</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );

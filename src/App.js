a9929ad077c46571ba8c77adc193b067fdd57f54
Oksana Kormilina <keysb@mail.ru>
2024-02-09 12:48:21 +0400

    Add redux state to authentication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import Counter from './components/Counter';
+import {useSelector} from 'react-redux'
 
+import Counter from './components/Counter';
+import Header from './components/Header'
+import Auth from './components/Auth'
+import UserProfile from './components/UserProfile'
 
 function App() {
-  return (
-    <Counter />
+const auth = useSelector((state) => state.auth.isAuthenticated)
+
+  return (<>
+  <Header />
+ {!auth && <Auth />} 
+ {auth && <UserProfile />} 
+ {auth &&  <Counter />}
+  </>
+   
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from '../store/index';
+
 import classes from './Header.module.css';
 
 const Header = () => {
+const dispatch = useDispatch()
+
+const auth = useSelector(state => state.auth.isAuthenticated)
+
+function handleLogout() {
+  dispatch(authActions.logout())
+}
+
+const navBar = (<nav>
+  <ul>
+    <li>
+      <a href='/'>My Products</a>
+    </li>
+    <li>
+      <a href='/'>My Sales</a>
+    </li>
+    <li>
+      <button onClick={handleLogout}>Logout</button>
+    </li>
+  </ul>
+</nav>)
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {auth && navBar}
     </header>
   );
 };
